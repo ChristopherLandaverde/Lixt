c9eae184498cf8864d603f433555ad15deb380a7
ChristopherLandaverde <christopherlandaverde@gmail.com>
2021-01-18 15:31:39 -0500

    Removed pymysql because it was a redudancy with flask_mysqldb

--- app.py
+++ app.py
@@ -5,32 +5,35 @@ import uuid
 from uuid import uuid4
 import pymysql
 
+app = Flask(__name__)
+
+
+#Trying Database Connection
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'land0627'
+app.config['MYSQL_DB'] = 'FLAPI'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 
 
 #Init App
-app = Flask(__name__)
-mysql = MySQL(app)
 
-#Ignore this
+mysql = MySQL(app)
 
 
 #Initalize Database Connection 
 def db_connection():
-    conn = None
     try:
-        conn = pymysql.connect(host="127.0.0.1",database="FLAPI",user="root",password='land0627',cursorclass=pymysql.cursors.DictCursor)
-        cursor = conn.cursor() 
-    except pymysql.Error as err:
-        print("Something went wrong: {}".format(err))
-    return conn
+        cur = mysql.connection.cursor()
+    except Exception as e:
+        raise(e)
+    return cur
         
 
 @app.route("/v1/",methods=["GET","POST","DELETE","PUT"])
 def gaitems():
     today = datetime.datetime.now()
-    conn = db_connection()
-    cursor=conn.cursor()
+    cursor = db_connection()
     
     if request.method == "GET":
         cursor.execute("SELECT * FROM Grocery_List")
@@ -53,7 +56,8 @@ def gaitems():
 
         cursor.execute("""INSERT INTO Grocery_List (name,createdBy,ID,createdAt)
         VALUES(%s,%s,%s,%s)""",(new_item,item_created_by,uuid.uuid4(),today))
-        conn.commit()
+        mysql.connection.commit()
+        cursor.close()
         return ("Item has been added succesfully"),200
             
     
@@ -61,14 +65,16 @@ def gaitems():
         new_item=request.form['name']
         item_created_by=request.form['createdBy']
         cursor.execute("""DELETE FROM Grocery_List WHERE name=%s and createdBy=%s""",(new_item,item_created_by))
-        conn.commit()
+        mysql.connection.commit()
+        cursor.close()
         return ("Item has been deleted succesfully"),200
     
     if request.method == "PUT":
         new_item=request.form['name']
         item_created_by=request.form['createdBy']
         cursor.execute("""UPDATE Grocery_List SET name=%s WHERE createdBy=%s""",(new_item,item_created_by))
-        conn.commit()
+        mysql.connection.commit()
+        cursor.close()
         return "Item has been edited succesfully",200
     
 
@@ -76,12 +82,11 @@ def gaitems():
 @app.route("/v1/<createdBy>",methods=["GET","DELETE"])
 def single_item(createdBy):
     today = datetime.datetime.now()
-    conn = db_connection()
-    cursor=conn.cursor()
+    cursor = db_connection() 
     book = None
     
     if request.method == "GET":
-        cursor.execute("""SELECT * FROM Grocery_List WHERE createdBy=%s""",(createdBy))
+        cursor.execute("""SELECT * FROM Grocery_List WHERE createdBy='%s'""" % (createdBy))
         items=[
             dict(id=row['ID'],userID=row['UserID'],name=row['name'],createdAt=row['createdAt'],createdBy=row['createdBy'],lastEdited=row['lastEdited'],lastEditedBy=row['lastEditedBy'])
             for row in cursor.fetchall()]
