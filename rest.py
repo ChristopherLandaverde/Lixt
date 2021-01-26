edf43459512d06a2425873879c31af78423a25b5
ChristopherLandaverde <christopherlandaverde@gmail.com>
2021-01-25 19:56:28 -0500

    Commiting the changes to this file

--- rest.py
+++ rest.py
@@ -1,24 +1,29 @@
-import pymysql
-from uuid import uuid4
-from functools import wraps
-import datetime
 import uuid
-from app import app
-from db import mysql,db_connection
+import datetime
 from flask import jsonify,request
+from app import app
+
+
+from db import mysql,db_connection
 
 
 
 
+def form(x):
+    data = request.get_json(force=True)
+    return data[x]
 
+def cur(*args):
+    cursor = db_connection()
+    cursor.execute(*args)
+    mysql.connection.commit()
+    cursor.close()
     
 
-@app.route("/v1/groceries",methods=["GET"])
-def get_gaitems():
-    if request.mimetype == 'application/json':
-        today = datetime.datetime.now()
+def fetchall_cur(x):  
+    if x:
         cursor = db_connection()
-        cursor.execute("SELECT * FROM Grocery_List")
+        cursor.execute(x)
         row = cursor.fetchall()
         resp = jsonify(row)
         resp.status_code=200
@@ -28,20 +33,23 @@ def get_gaitems():
         resp.status_code = 500
         return resp
 
+    
 
-        
 
+
+@app.route("/v1/groceries",methods=["GET"])
+def get_gaitems():
+    if request.mimetype == 'application/json':
+        all_items =fetchall_cur("""SELECT * FROM Grocery_List""")
+        return all_items
+
+    
 @app.route("/v1/groceries",methods=["POST"])
 def post_gaitems():
         if request.mimetype == 'application/json':
-            data = request.get_json(force=True)
-            today = datetime.datetime.now()
-            new_item=data['name']
-            item_created_by=data['createdBy']
-            cursor = db_connection()
-            cursor.execute("""INSERT INTO Grocery_List (name,createdBy,ID,createdAt) VALUES(%s,%s,%s,%s)""",(new_item,item_created_by,uuid.uuid4(),today))
-            mysql.connection.commit()
-            cursor.close()
+            new_item=form('name')
+            item_created_by=form('createdBy')
+            x = cur("""INSERT INTO Grocery_List (name,createdBy,ID,createdAt) VALUES(%s,%s,%s,%s)""",(new_item,item_created_by,uuid.uuid4(),datetime.datetime.now()))
             return ("Item has been added succesfully"),200
             
             
@@ -49,25 +57,17 @@ def post_gaitems():
 @app.route("/v1/groceries",methods=["DELETE"])    
 def delete_gaitems():
     if request.mimetype == 'application/json':
-        data = request.get_json(force=True)
-        new_item=data['name']
-        item_created_by=data['createdBy']
-        cursor = db_connection()
-        cursor.execute("""DELETE FROM Grocery_List WHERE name=%s and createdBy=%s""",(new_item,item_created_by))
-        mysql.connection.commit()
-        cursor.close()
+        new_item=form('name')
+        item_created_by=form('createdBy')
+        x = cur("""DELETE FROM Grocery_List WHERE name=%s and createdBy=%s""",(new_item,item_created_by))
         return ("Item has been deleted succesfully"),200
 
 @app.route("/v1/groceries/createdBy",methods=["PUT"])    
 def put_gaitems():
     if request.mimetype == 'application/json':
-        cursor = db_connection()
-        data = request.get_json(force=True)
-        new_item=data['name']
-        item_created_by=data['createdBy']
-        cursor.execute("""UPDATE Grocery_List SET name=%s WHERE createdBy=%s""",(new_item,item_created_by))
-        mysql.connection.commit()
-        cursor.close()
+        new_item=form('name')
+        item_created_by=form('createdBy')
+        cur("""UPDATE Grocery_List SET name=%s WHERE createdBy=%s""",(new_item,item_created_by))
         return "Item has been edited succesfully",200
     
 
@@ -77,20 +77,12 @@ def single_item():
     try:
         id = request.args.get('createdBy')
         if id:
-            today = datetime.datetime.now()
-            cursor = db_connection() 
-            cursor.execute("""SELECT * FROM Grocery_List WHERE createdBy='%s'""" % (id))
-            row = cursor.fetchall()
-            resp = jsonify(row)
-            resp.status_code=200
-            return resp
-        else:
-            resp = jsonify('User "id" not found in query string')
-            resp.status_code = 500
-            return resp
+            user_items = fetchall_cur("""SELECT * FROM Grocery_List WHERE createdBy='%s'""" % (id))
+            return user_items
 
     except Exception as e:
         print(e)
+
 
 #server
 if __name__ == '__main__':
