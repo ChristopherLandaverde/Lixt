15ae962432cebcb9385cc5a1bf73aa9ba10e963c
ChristopherLandaverde <christopherlandaverde@gmail.com>
2021-02-09 17:39:12 -0500

    Used a plugin for the pytest to create a mockserver

--- src/testapp.py
+++ src/testapp.py
@@ -1,49 +1,41 @@
 
-import json
+import time
+import pytest
 import requests
+
 
 URL = 'http://127.0.0.1:5000'
 
+def sleep_two(*args, **kwargs):
+    time.sleep(2)
 
-def test_post_page():
-    data = {'name': 'Machine'}
-    headers = {'Content-type': 'application/json',
-               'Accept': 'text/plain', 'userID': 'Milan'}
-    testing_post_request = requests.post(URL + '/v1/groceries',
-            data=json.dumps(data), headers=headers)  # Assumses that it has a path of "/"
-    assert testing_post_request.status_code == 200  # Assumes that it will return a 200 responsed
+@pytest.mark.server(url='/v1/groceries/', response=[{'name': 'Machine'}], method='POST',callback=sleep_two)
+def test_handler_responses():
+    with pytest.raises(requests.exceptions.Timeout):
+        response = requests.post(URL +'/v1/groceries/',timeout=1)
+        assert response.status_code == 200
 
+@pytest.mark.server(url='/v1/groceries/', response=[{'name': 'Machine'}], method='GET',callback=sleep_two)
+def test_post_responses():
+    with pytest.raises(requests.exceptions.Timeout):
+        response = requests.get('http://127.0.0.1:5000'+'/v1/groceries',timeout=1)
+        assert response.status_code == 200
+        assert response.json() == [{'name': 'Machine'}]
 
-def test_get_page():
-    data = {'name': 'Machine'}
-    headers = {'Content-type': 'application/json',
-               'Accept': 'text/plain'}
-    testing_get_request = requests.get(URL + '/v1/groceries',
-            data=json.dumps(data), headers=headers)  # Assumses that it has a path of "/"
-    assert testing_get_request.status_code == 200  # Assumes that it will return a 200 responsed
+@pytest.mark.server(url='/v1/users/', response=[{'userID': 'Milan'}], method='GET')
+def test_user_responses():
+        response= requests.get(URL +'/v1/users/')
+        assert response.status_code == 200
+        assert response.json() == [{'userID': 'Milan'}]
 
+@pytest.mark.server(url='/v1/groceries/', response=[{'name': 'Machine'}], method='DELETE')
+def test_delete_responses():
+        response= requests.get(URL+'/v1/groceries/')
+        assert response.status_code == 200
+        assert response.json() == [{'name': 'Machine'}]
 
-def test_user_page():
-    data = {'userID': 'Milan'}
-    headers = {'Content-type': 'application/json',
-               'Accept': 'text/plain'}
-    testing_user_request = requests.get(URL + '/v1/users',
-            params=data, headers=headers)  # Assumses that it has a path of "/"
-    assert testing_user_request.status_code == 200  # Assumes that it will return a 200 responsed
-
-def test_put_page():
-    data = {'name': 'Fried Chicken'}
-    headers = {'Content-type': 'application/json',
-               'Accept': 'text/plain', 'userID': 'Milan'}
-    testing_put_request = requests.put(URL + '/v1/groceries',
-            data=json.dumps(data), headers=headers)  # Assumses that it has a path of "/"
-    assert testing_put_request.status_code == 200  # Assumes that it will return a 200 responsed
-
-
-def test_delete_page():
-    data = {'name': 'Fried Chicken'}
-    headers = {'Content-type': 'application/json',
-               'Accept': 'text/plain', 'userID': 'Milan'}
-    testing_delete_request = requests.delete(URL + '/v1/groceries',
-            data=json.dumps(data), headers=headers)  # Assumses that it has a path of "/"
-    assert testing_delete_request.status_code == 200  # Assumes that it will return a 200 responsed
+@pytest.mark.server(url='/v1/groceries/', response=[{'name': 'Fried Chicken','userID':'Milan'}], method='PUT')
+def test_put_responses():
+        response= requests.put(URL+'/v1/groceries/')
+        assert response.status_code == 200
+        assert response.json() == [{'name': 'Fried Chicken','userID':'Milan'}]
