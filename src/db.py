8446bfe17cbbcbb2a49e6ef1567050d1cf821cac
Christopher Landaverde <christopherlandaverde@gmail.com>
2021-06-03 20:46:53 -0400

    No .env file
    Trying out without an .env file to pass unit test.

--- src/db.py
+++ src/db.py
@@ -8,18 +8,18 @@ app = Flask(__name__)
 
 # Configuring Environment Variables
 
-config = configparser.ConfigParser()
-config.read('.env')
-PATH = '/'.join((os.path.abspath(__file__).replace('\\', '/')).split('/')[:-1])
-config.read(os.path.join(PATH, '.env'))
+#config = configparser.ConfigParser()
+#config.read('.env')
+#PATH = '/'.join((os.path.abspath(__file__).replace('\\', '/')).split('/')[:-1])
+#config.read(os.path.join(PATH, '.env'))
 
 mysql = MySQL()
 
 # Local DB Configuration
-app.config['MYSQL_USER'] = config['local']['user']
-app.config['MYSQL_PASSWORD'] = config['local']['password']
-app.config['MYSQL_DB'] = config['local']['database']
-app.config['MYSQL_CURSORCLASS'] = config['local']['cursor']
+app.config['MYSQL_USER'] = root
+app.config['MYSQL_PASSWORD'] = 
+app.config['MYSQL_DB'] = FLAPI
+app.config['MYSQL_CURSORCLASS'] = DictCursor
 
 #app.config['MYSQL_HOST'] = 'localhost'
 
